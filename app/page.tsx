eede48ad5b64010ae71a48473755fead0ba74296
borzcodes <realmahmoudcodes@gmail.com>
2026-04-26 20:51:04 +0100

    'Initializing CoinView'

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,31 @@
-import React, { Suspense } from 'react';
-import CoinOverview from '@/components/home/CoinOverview';
-import TrendingCoins from '@/components/home/TrendingCoins';
-import {
-    CategoriesFallback,
-    CoinOverviewFallback,
-    TrendingCoinsFallback,
-} from '@/components/home/fallback';
-import Categories from '@/components/home/Categories';
+import React from 'react'
+import Image from "next/image";
 
-const Page = async () => {
+const Page = () => {
     return (
         <main className="main-container">
-            <section className="home-grid">
-                <Suspense fallback={<CoinOverviewFallback />}>
-                    <CoinOverview />
-                </Suspense>
+             <section className='home-grid'>
+                 <div id='coin-overview'>
+                    <div className='header pt-2'>
+                        <Image src='https://assets.coingecko.com/coins/images/1/large/bitcoin.png'
+                               alt='Bitcoin'
+                               height={56}
+                               width={56}
+                        />
+                        <div className='info'>
+                            <p>Bitcoin / BTC</p>
+                            <h1>$76,800.00</h1>
+                        </div>
+                    </div>
+                 </div>
 
-                <Suspense fallback={<TrendingCoinsFallback />}>
-                    <TrendingCoins />
-                </Suspense>
-            </section>
+                 <p>Trending Coins</p>
+             </section>
 
-            <section className="w-full mt-7 space-y-4">
-                <Suspense fallback={<CategoriesFallback />}>
-                    <Categories />
-                </Suspense>
+            <section className='w-full mt-7 space-y-4'>
+                <p>Categories</p>
             </section>
         </main>
-    );
-};
-
-export default Page;
+    )
+}
+export default Page
